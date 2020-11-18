a0467967bd55a46302fe5d8df233744d962c8089
ehrhorn <pqc735@alumni.ku.dk>
2020-11-18 20:42:26 +0100

    Making powershovel small dbs.

--- cubedb/make_powershovel_dbs.py
+++ cubedb/make_powershovel_dbs.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from cubedb.save_random_events import random_events_saver
 
 datasets_root = Path().home() / "work" / "datasets"
-datasets = [f for f in datasets_root.iterdir()]
+datasets = [f for f in datasets_root.iterdir() if f.is_dir()]
 print(f"{datetime.now()}: Looking through datasets")
 for dataset in datasets:
     powershovel_db = dataset / "meta" / "powershovel_events.db"
